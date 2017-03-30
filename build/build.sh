b27cf1d35cb50c8951528fc75f33730666e9a3d9
brent-edwards-online <brent@brentedwardsonline.com>
2017-03-31 09:37:50 +1000

    Add new build script

---
+++ build/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function mydelay()
+{
+	printf "  "
+	for (( i = 1 ; i <= $1 ; i++ )); do
+		printf "*"
+		sleep 0.5
+	done
+	echo ""
+}
+
+ANGULAR_DIR="../../brentedwardsonline_angular2"
+PHP_API="../../brentedwardsonline_phpapi"
+
+clear
+echo "Building BrentEdwardsOnline Angular2" 
+rm -rf dist/
+mydelay 5
+echo ""
+
+echo "Compiling Angular2 Application" 
+echo ""
+pushd $ANGULAR_DIR
+echo ""
+ng build --prod
+popd
+
+echo "Adding Php Api" 
+mydelay 5
+cp -r ../../brentedwardsonline_phpapi/api/ dist/
+rm dist/api/includes/config_to_save.php
+
+echo "Zipping Files" 
+mydelay 5
+DATE_STRING="`date +%Y%m%d_%H%M%S`"
+pushd dist/
+zip -r "../beol_ng2_$DATE_STRING.zip" ./*
+popd
+echo ""
+echo "  Finished Building BrentEdwardsOnline Angular2" 
+mydelay 5
+echo "  Filename: beol_$DATE_STRING.zip"
